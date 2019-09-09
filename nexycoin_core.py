aec5a1219206b72de95acbb8e97fd108048d49fb
Luke Gniwecki <38128190+lukegniwecki@users.noreply.github.com>
2019-09-09 21:38:53 +0100

    Update nexycoin_core.py
    Removed a few comments

--- nexycoin_core.py
+++ nexycoin_core.py
@@ -1,8 +1,3 @@
-# Required:
-# Flask==-0.12.2: pip install Flask==0.12.2
-# Postman HTTP Client: https://www.getpostman.com
-# requests==2.18.4: pip install requests==2.18.4
-
 # Library import
 import datetime 
 import hashlib 
